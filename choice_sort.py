4b848625690a3514049f51ea44b0d8826d700679
axel-lord <art.a.annenkov@gmail.com>
2025-04-19 08:27:12 +0300

    добавил логирование в файл для метода sort, добавил мок, заменяющий логирование на print в консоль

--- choice_sort.py
+++ choice_sort.py
@@ -1,10 +1,33 @@
 # choice_sort.py
 
-def sort(list):
-    for i in range(len(list)):
-        for j in range(i):
-            if list[j] > list[i]:
-                buf = list.pop(i)
-                list.insert(j, buf)
+from datetime import datetime
+import logging
 
-    return list
+class Sorter():
+    # create logger with 'spam_application'
+    logger = logging.getLogger('lang_helper')
+    logger.setLevel(logging.INFO)
+    # create file handler which logs even debug messages
+    logging.basicConfig(filename='log.log', encoding='utf-8', level=logging.INFO)
+
+    def log(self, *text):
+        mess = str(datetime.now().strftime('%d-%m-%Y %H:%M'))
+        mess += ': '
+        for line in text:
+            mess += ' ' + str(line)
+
+        logging.info(mess)
+        print(mess)
+
+        return
+
+    def sort(self, in_list):
+        for i in range(len(in_list)):
+            for j in range(i):
+                if in_list[j] > in_list[i]:
+                    buf = in_list.pop(i)
+                    in_list.insert(j, buf)
+
+        self.log('choice sort:', in_list)  # логируем запись о действии в файл
+
+        return in_list
--- unit_test.py
+++ unit_test.py
@@ -1,27 +1,34 @@
 # unit_test.py
 
 import unittest
-from choice_sort import sort
+from choice_sort import Sorter
 
 class TestSort(unittest.TestCase):
     def test_sort(self):
+        sorter = Sorter()
+
+        sorter.log = self.mock_log
+
         # Тест на пустом массиве
-        self.assertEqual(sort([]), [])
+        self.assertEqual(sorter.sort([]), [])
         
         # Тест на массиве из одного элемента
-        self.assertEqual(sort([22]), [22])
+        self.assertEqual(sorter.sort([22]), [22])
         
         # Тест на массиве из нескольких элементов
-        self.assertEqual(sort([64, 34, 25, 12, 22, 11, 90]), [11, 12, 22, 25, 34, 64, 90])
+        self.assertEqual(sorter.sort([64, 34, 25, 12, 22, 11, 90]), [11, 12, 22, 25, 34, 64, 90])
         
         # Тест на массиве с отрицательными числами
-        self.assertEqual(sort([-5, -1, -3, -2, -4]), [-5, -4, -3, -2, -1])
+        self.assertEqual(sorter.sort([-5, -1, -3, -2, -4]), [-5, -4, -3, -2, -1])
         
         # Тест на массиве с повторяющимися элементами
-        self.assertEqual(sort([5, 3, 5, 2, 8, 2]), [2, 2, 3, 5, 5, 8])
+        self.assertEqual(sorter.sort([5, 3, 5, 2, 8, 2]), [2, 2, 3, 5, 5, 8])
         
         # Тест на уже отсортированном массиве
-        self.assertEqual(sort([1, 2, 3, 4, 5]), [1, 2, 3, 4, 5])
+        self.assertEqual(sorter.sort([1, 2, 3, 4, 5]), [1, 2, 3, 4, 5])
         
         # Тест на массиве, отсортированном в обратном порядке
-        self.assertEqual(sort([5, 4, 3, 2, 1]), [1, 2, 3, 4, 5])
+        self.assertEqual(sorter.sort([5, 4, 3, 2, 1]), [1, 2, 3, 4, 5])
+
+    def mock_log(self, *text):
+        print(*text)
